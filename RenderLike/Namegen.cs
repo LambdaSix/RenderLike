bfad3af54c604b893e52bed9a7ac7e2ac61f4d37
Nicola <LambdaSix@users.noreply.github.com>
2016-06-16 16:58:09 +0100

    Reorganise project structure.

---
+++ RenderLike/Namegen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RenderLike
+{
+	/*
+	"Configuration": {
+		"SyllablePre":  "$P", 
+		"SyllableStart": "$s",
+		"SyllableMiddle": "$m",
+		"SyllableEnd": "$e",
+		"SyllablePost": "$p",
+		"SyllableVocal": "$v",
+		"SyllableConsonant": "$c",
+		"SyllablePhoneme": "$?",
+		"RuleChancePrefix": "%"
+	}
+	*/
+
+	public class Namegen
+	{
+		private Rand _rand = new Rand();
+
+		private Dictionary<string,NameGeneratorDefinition> NameGenerators { get; set; }
+
+		public string GenerateFromRule(string ruleName)
+		{
+			return "";
+		}
+
+		private string PickSyllableStart(string ruleName)
+		{
+		    //return GetGenerator(ruleName).SyllableStart
+            throw new NotImplementedException();
+		}
+
+		private string PickSyllableMiddle(string ruleName)
+		{
+            //var pickList = NameGenerators.FirstOrDefault()
+            throw new NotImplementedException();
+        }
+
+		private string PickSyllableEnd(string ruleName)
+		{
+            throw new NotImplementedException();
+        }
+
+		private NameGeneratorDefinition GetGenerator(string ruleName)
+		{
+			NameGeneratorDefinition outValue;
+			return NameGenerators.TryGetValue(ruleName, out outValue)
+				? outValue
+				: default(NameGeneratorDefinition);
+		}
+	}
+
+	/// <summary>
+	/// JSON data container
+	/// </summary>
+	internal class NameGeneratorDefinition
+	{
+		public List<string> SyllableStart { get; set; }
+		public List<string> SyllablesMiddle { get; set; }
+		public List<string> SyllablesEnd { get; set; }
+		public List<string> Illegal { get; set; }
+		public List<string> Rules { get; set; }
+	}	
+}
