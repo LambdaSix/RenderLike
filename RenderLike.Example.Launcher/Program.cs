c542cd34507df30547d4fbf2a7bb0dec46f3c2bf
Nicola <LambdaSix@users.noreply.github.com>
2016-06-22 16:16:37 +0100

    Add example project
    Simple example showing RenderLike working.

---
+++ RenderLike.Example.Launcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderLike.Example.Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameInfo = new GameInfo()
+            {
+                Font = "celtic_garamond_10x10_gs_tc.png",
+                FontLayout = FontLayout.TCOD,
+                FontType = FontType.GreyscaleAA,
+                Height = 24,
+                Width = 80
+            };
+            var game = new ExampleGame(gameInfo);
+
+            game.Start();
+        }
+    }
+}
